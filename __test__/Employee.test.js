13bd7c8c0e40e16b2a17c187f8a3c25752160d9b
Cody Murray <cdillon.murray@yahoo.com>
2022-04-12 11:03:38 -0500

    added files and folder structure

---
+++ __test__/Employee.test.js
@@ -0,0 +1,10 @@
+// for each class, which will start with the employee class
+
+// right test for constructor for the employee class 
+// test for consructor to fail
+// right code to make constructor pass
+// refactor code 
+
+
+// then follow same structure,
+//  write constructor for manager, enginer, student class
