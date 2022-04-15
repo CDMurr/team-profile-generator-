a0266ee6a560bd6216540ad5d46d37b7600fa20c
Cody Murray <cdillon.murray@yahoo.com>
2022-04-15 13:32:45 -0500

    got test to pass for class, name, and id

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -2,12 +2,36 @@
 const Employee = require('../lib/Employee')
 // right test for constructor for the employee class 
 test('creates a employee badge', () => {
-    const employee = new Employee('dillon', 899, 'email@email.com')
+    const employee = new Employee('dillon', 1212, 'email@email.com')
    
     expect(employee.name).toBe('dillon');
-    expect(employee.id).toBe(899);
+    expect(employee.id).toBe(1212);
     expect(employee.email).toBe("email@email.com");
 });
+
+test('gets the employee name', () => {
+    const employee = new Employee('dillon', 1212, 'email@email.com')
+     
+    expect(employee.getName()).toBe('dillon');
+}); 
+
+test('gets employee id', () => {
+    const employee = new Employee('dillon', 1212, 'email@email.com')
+    const id = "1212"
+    expect(employee.getId()).toBe(1212)
+});
+
+
+
+// test('gets employee email', () => {
+//     const employee = new Employee('code@email.com');
+    
+//     expect(employee.getEmail()).toBe('code@email.com')
+// })
+
+
+
+
 
 // test for consructor to fail
 // right code to make constructor pass
