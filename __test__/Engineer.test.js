6bed07d9971730b30e7a0dabd296320f6add00ec
Cody Murray <cdillon.murray@yahoo.com>
2022-04-16 14:53:57 -0500

    test pass for engineer

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,13 +1,19 @@
 const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 
+
 test("creates a engineer profile", () => {
-    const engineer = new engineer('dillon', 1212, 'email@email.com', '979-265-7052')
+    const engineer = new Engineer('dillon', 1212, 'email@email.com', 'CDMurr')
 
     expect(engineer.name).toBe('dillon');
     expect(engineer.id).toBe(1212);
     expect(engineer.email).toBe("email@email.com")
-    expect(engineer.officeNumber).toBe('979-265-7052')
+    expect(engineer.gitHub).toBe('CDMurr')
 });
 
 // test for github username 
+test('gets employee role', () => {
+    const engineer = new Engineer('Engineer')
+
+    expect(engineer.getRole()).toBe('Engineer')
+});
